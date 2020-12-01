0f919726eb40eda9ffbaacab6c0f9e30a256b6c0
Alexander Perez <alexander.perez@patelhs.org>
2020-12-01 13:56:40 +0000

    done

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+  String[] roster1 = {"Barcelona, Real Madrid, Manchester United, Manchester City, Liverpool, France, Arsenal, Milan, Brazil, Argentina."};
+
+    for(int a = 0; 1 < 10; a++)
+    System.out.print(roster1[a]);
+
+    String[]roster2 = new String[10];
+
+    for(int a = 0; a < roster1.length; a++)
+
+  System.out.println("\nRoster 1 is now copied to Roster 2.");
+
   }
 }
